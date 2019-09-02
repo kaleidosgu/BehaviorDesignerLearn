62923ff108c623ad5938cdcc2941b9f3dc46fc16
kaleidosgu <gjcaoyi@163.com>
2019-09-02 17:06:00 +0800

    重新创建工程

---
+++ LearnBDesigner/Assets/Scripts/TestConditional.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+
+public class TestConditional : Conditional {
+
+    public SharedTransform SharedTarget;
+    public override TaskStatus OnUpdate()
+    {
+        return TaskStatus.Failure;
+    }
+}
