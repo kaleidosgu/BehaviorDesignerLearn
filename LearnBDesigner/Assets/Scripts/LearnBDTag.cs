12bad6774001842dde9b3d1bd613fa13db184a46
kaleidosgu <gjcaoyi@163.com>
2019-09-04 11:18:20 +0800

    删除文件

---
+++ LearnBDesigner/Assets/Scripts/LearnBDTag.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearnBDTag  {
+    public static string LearnDBTag_Target = "Target";
+}
---
+++ LearnBDesigner/Assets/Scripts/TestGizmos.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestGizmos : MonoBehaviour {
+    public float LengthOfGizmos;
+    public LayerMask RelatedLayerMask;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Vector3 vecDst = transform.right.normalized * LengthOfGizmos + transform.position;
+        Gizmos.DrawLine(transform.position, vecDst);
+
+        RaycastHit raycastHit;
+        bool bRes = Physics.Raycast(transform.position, transform.right, out raycastHit, LengthOfGizmos, (int)RelatedLayerMask);
+        if (bRes == true)
+        {
+            int a = 0;
+        }
+    }
+}
